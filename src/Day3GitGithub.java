c99b687de6f0dca58bc721f67563eb5c079d3b61
Furkan Kenger <kengermf@gmail.com>
2022-08-16 21:18:28 +0300

    V3

---
+++ src/Day3GitGithub.java
@@ -0,0 +1,12 @@
+public class Day3GitGithub {
+    public static void main(String[] args) {
+        System.out.println("3. gün 1. Deneme");
+        System.out.println("3. gün 2. Deneme");
+        System.out.println("3. gün 3. Branch Deneme");
+        System.out.println("3. gün 4. Master Deneme");
+        System.out.println("3. gün 5. Master Deneme");
+        System.out.println("3. gün 6. Branch Deneme");
+        System.out.println("3. gün 7. Branch Deneme");
+
+    }
+}
