32c91d1bc907668385a722ab263298af86b90dfb
FurkanKenger <108692217+FurkanKenger@users.noreply.github.com>
2022-08-16 22:11:55 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,7 +2,13 @@ package Day3GitGithub.src;
 
 public class Day1Git {
     public static void main(String[] args) {
-        System.out.println("Hello Git 1. Deneme");
-        System.out.println("Hello Git 1. Deneme");
+         System.out.println("Hello Git 1. Deneme");
+         System.out.println("Hello Git 2. Deneme");
+         System.out.println("Hello Git 3. Deneme");
+         System.out.println("Hello Git 4. Deneme");
+         System.out.println("Hello Git 5. Deneme");
+         System.out.println("Hello Git 6. Deneme");
+         System.out.println("Hello Git 7. Deneme");
+         System.out.println("Hello Git 8. Deneme");
     }
 }
